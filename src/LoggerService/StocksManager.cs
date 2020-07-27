088ce1ab8265e43423a0b8eb8b4be2df47d55850
Paulo H V Neves <paulodemoc@gmail.com>
2020-07-27 14:44:11 -0300

    adjustments for localhost usage

--- src/LoggerService/StocksManager.cs
+++ src/LoggerService/StocksManager.cs
@@ -17,38 +17,40 @@ namespace StocksService
 
         public StocksManager(IDependenciesManager _dependencies)
         {
-            stocksValues = new List<StockValue>();
-            Task.Run(() =>
-            {
-                using (var client = new WebsocketClient(new Uri(_dependencies.stocksendpoint)))
-                {
-                    client.ReconnectTimeout = TimeSpan.FromSeconds(30);
-                    //client.ReconnectionHappened.Subscribe(info =>
-                    //    Log.Information($"Reconnection happened, type: {info.Type}"));
+            stocksValues = new List<StockValue>() { new StockValue { StockCode = "ABC", Value = 5 },
+            new StockValue { StockCode = "DEF", Value = 10 },
+            new StockValue { StockCode = "GHI", Value = 15 }};
+            //Task.Run(() =>
+            //{
+            //    using (var client = new WebsocketClient(new Uri(_dependencies.stocksendpoint)))
+            //    {
+            //        client.ReconnectTimeout = TimeSpan.FromSeconds(30);
+            //        //client.ReconnectionHappened.Subscribe(info =>
+            //        //    Log.Information($"Reconnection happened, type: {info.Type}"));
 
-                    client.MessageReceived.Subscribe(msg =>
-                    {
-                        try
-                        {
-                            var value = JsonConvert.DeserializeObject<Dictionary<string, double>>(msg.ToString());
-                            StockValue stock = stocksValues.FirstOrDefault(s => s.StockCode.Equals(value.Keys.ElementAt(0))) ?? new StockValue { StockCode = value.Keys.ElementAt(0) };
-                            if (stock.Timestamp < value["timestamp"])
-                            {
-                                stock.Value = value[stock.StockCode];
-                                stock.Timestamp = value["timestamp"];
-                                if (!stocksValues.Any(s => s.StockCode.Equals(value.Keys.ElementAt(0))))
-                                    stocksValues.Add(stock);
-                            }
-                        }
-                        catch (Exception ex)
-                        {
-                            throw ex;
-                        }
-                    });
-                    client.Start();
-                    new ManualResetEvent(false).WaitOne();
-                }
-            });
+            //        client.MessageReceived.Subscribe(msg =>
+            //        {
+            //            try
+            //            {
+            //                var value = JsonConvert.DeserializeObject<Dictionary<string, double>>(msg.ToString());
+            //                StockValue stock = stocksValues.FirstOrDefault(s => s.StockCode.Equals(value.Keys.ElementAt(0))) ?? new StockValue { StockCode = value.Keys.ElementAt(0) };
+            //                if (stock.Timestamp < value["timestamp"])
+            //                {
+            //                    stock.Value = value[stock.StockCode];
+            //                    stock.Timestamp = value["timestamp"];
+            //                    if (!stocksValues.Any(s => s.StockCode.Equals(value.Keys.ElementAt(0))))
+            //                        stocksValues.Add(stock);
+            //                }
+            //            }
+            //            catch (Exception ex)
+            //            {
+            //                throw ex;
+            //            }
+            //        });
+            //        client.Start();
+            //        new ManualResetEvent(false).WaitOne();
+            //    }
+            //});
         }
     }
 }
